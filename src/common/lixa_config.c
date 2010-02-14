c7eab43b7ae26d4d62c006e910a9b8ae5393b851
tiian <tiian@39947302-e88a-4d6c-85f1-75da6402909b>
2010-02-14 22:08:56 +0000

    Intermediate commit...

--- src/common/lixa_config.c
+++ src/common/lixa_config.c
@@ -259,3 +259,30 @@ int lixa_config_digest(int fd, md5_digest_hex_t digest)
                 "ret_cod=%d/errno=%d\n", excp, ret_cod, errno));
     return ret_cod;
 }
+
+
+
+int lixa_job_set_path_profile(lixa_job_t *job, const char *path,
+                              const char *profile)
+{
+    enum Exception { NONE } excp;
+    int ret_cod = LIXA_RC_INTERNAL_ERROR;
+    
+    LIXA_TRACE(("\n"));
+    TRY {
+        
+        THROW(NONE);
+    } CATCH {
+        switch (excp) {
+            case NONE:
+                ret_cod = LIXA_RC_OK;
+                break;
+            default:
+                ret_cod = LIXA_RC_INTERNAL_ERROR;
+        } /* switch (excp) */
+    } /* TRY-CATCH */
+    LIXA_TRACE(("/excp=%d/"
+                "ret_cod=%d/errno=%d\n", excp, ret_cod, errno));
+    return ret_cod;
+}
+
--- src/server/server_config.c
+++ src/server/server_config.c
@@ -201,7 +201,6 @@ int server_parse(struct server_config_s *sc,
 {
     enum Exception { PARSE_LISTENER_ERROR
                      , PARSE_MANAGER_ERROR
-                     , PROFILE_MANAGER_ERROR
                      , SERVER_PARSE_ERROR
                      , NONE } excp;
     int ret_cod = LIXA_RC_INTERNAL_ERROR;
@@ -222,11 +221,6 @@ int server_parse(struct server_config_s *sc,
                     if (LIXA_RC_OK != (ret_cod = server_parse_manager(
                                            sc, tpa, cur_node)))
                         THROW(PARSE_MANAGER_ERROR);
-                } else if (!xmlStrcmp(cur_node->name,
-                                      LIXA_XML_CONFIG_PROFILE)) {
-                    if (LIXA_RC_OK != (ret_cod = server_parse_profile(
-                                           sc, tpa, cur_node)))
-                        THROW(PROFILE_MANAGER_ERROR);
                 }
             }
             if (LIXA_RC_OK != (ret_cod = server_parse(
@@ -239,7 +233,6 @@ int server_parse(struct server_config_s *sc,
         switch (excp) {
             case PARSE_LISTENER_ERROR:
             case PARSE_MANAGER_ERROR:
-            case PROFILE_MANAGER_ERROR:
             case SERVER_PARSE_ERROR:
                 break;
             case NONE:
@@ -420,33 +413,6 @@ int server_parse_manager(struct server_config_s *sc,
 
 
     
-int server_parse_profile(struct server_config_s *sc,
-                         struct thread_pipe_array_s *tpa,
-                         xmlNode *a_node)
-{
-    enum Exception { NONE } excp;
-    int ret_cod = LIXA_RC_INTERNAL_ERROR;
-    
-    LIXA_TRACE(("server_parse_profile\n"));
-    TRY {
-        
-        THROW(NONE);
-    } CATCH {
-        switch (excp) {
-            case NONE:
-                ret_cod = LIXA_RC_OK;
-                break;
-            default:
-                ret_cod = LIXA_RC_INTERNAL_ERROR;
-        } /* switch (excp) */
-    } /* TRY-CATCH */
-    LIXA_TRACE(("server_parse_profile/excp=%d/"
-                "ret_cod=%d/errno=%d\n", excp, ret_cod, errno));
-    return ret_cod;
-}
-
-
-
 void server_config_init(struct server_config_s *sc,
                         struct thread_pipe_array_s *tpa)
 {
