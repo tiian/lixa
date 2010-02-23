113f21c379f85b31c7f533396262d760229a1e11
tiian <tiian@39947302-e88a-4d6c-85f1-75da6402909b>
2010-02-23 22:13:28 +0000

    This is basically a gold plating commit. The copyright header statement was changed in every file: now only GPL is allowed as license.

--- src/server/server_status.h
+++ src/server/server_status.h
@@ -1,34 +1,20 @@
 /*
- * Copyright (c) 2009, Christian Ferrari
+ * Copyright (c) 2009-2010, Christian Ferrari <tiian@users.sourceforge.net>
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
+ * This file is part of LIXA.
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the names of the copyright holders nor the names of its
- *    contributors may be used to endorse or promote products derived from
- *    this software without specific prior written permission.
+ * LIXA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
  *
- * Alternatively, this software may be distributed under the terms of the
- * GNU General Public License ("GPL") version 2 as published by the Free 
- * Software Foundation.
+ * LIXA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * You should have received a copy of the GNU General Public License
+ * along with LIXA.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef SERVER_STATUS_H
 # define SERVER_STATUS_H
@@ -791,74 +777,6 @@ extern "C" {
      * @return a<b => -1, a>b => +1, a=b =>0
      */
     int size_t_compare_func(gconstpointer a, gconstpointer b);
-
-
-
-    /**
-     * Initialize a structure of type @ref thread_status_s
-     * @param ts OUT reference to the structure must be initialized
-     * @param id IN thread id must assigned
-     * @param tpa IN reference to the thread pipe array
-     */
-    void thread_status_init(struct thread_status_s *ts,
-                            int id,
-                            struct thread_pipe_array_s *tpa);
-
-
-    
-    /**
-     * Load the files associated to memory mapped status
-     * @param ts IN/OUT pointer to the thread status structure
-     * @param status_file_prefix IN the prefix used for status files
-     * @return a reason code
-     */
-    int thread_status_load_files(struct thread_status_s *ts,
-                                 const char *status_file_prefix);
-
-    
-
-    /**
-     * Scan the status file and enqueue recovery pending transactions in
-     * the server recovery table
-     * @param ts IN/OUT thread status reference
-     * @param srt IN/OUT server recovery table reference
-     * @return a standardized return code
-     */
-    int thread_status_recovery(struct thread_status_s *ts,
-                               srvr_rcvr_tbl_t *srt);
-
-
-
-    /**
-     * Check a transaction header block and determines if it's in recovery
-     * pending state
-     * @param data IN transaction header block must be analyzed
-     * @return a boolean value
-     */
-    int thread_status_is_recovery_pending(
-        const struct status_record_data_s *data);
-
-
-    
-    /**
-     * Synchronize status files: this is the atomic operation necessary to
-     * guarantee transactionality property of the system
-     * @param ts IN thread status reference
-     * @return a reason code
-     */
-    int thread_status_sync_files(struct thread_status_s *ts);
-
-
-    
-    /**
-     * Remove all records from an updated records tree
-     * @param ur IN/OUT the reference to updated records GTree structure
-     */
-    static inline void thread_status_updated_records_clean(GTree *ur) {
-        LIXA_TRACE(("thread_status_updated_records_clean: cleaning "
-                    "tree allocated at %p\n", ur));
-        g_tree_foreach(ur, traverse_and_delete, ur);
-    }
 
 
 
---
+++ src/server/server_thread_status.h
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2009-2010, Christian Ferrari <tiian@users.sourceforge.net>
+ * All rights reserved.
+ *
+ * This file is part of LIXA.
+ *
+ * LIXA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * LIXA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LIXA.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef SERVER_THREAD_STATUS_H
+# define SERVER_THREAD_STATUS_H
+
+
+
+#include <config.h>
+
+
+
+#include <server_status.h>
+
+
+
+/* save old LIXA_TRACE_MODULE and set a new value */
+#ifdef LIXA_TRACE_MODULE
+# define LIXA_TRACE_MODULE_SAVE LIXA_TRACE_MODULE
+# undef LIXA_TRACE_MODULE
+#else
+# undef LIXA_TRACE_MODULE_SAVE
+#endif /* LIXA_TRACE_MODULE */
+#define LIXA_TRACE_MODULE      LIXA_TRACE_MOD_SERVER_STATUS
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+
+
+    /**
+     * Initialize a structure of type @ref thread_status_s
+     * @param ts OUT reference to the structure must be initialized
+     * @param id IN thread id must assigned
+     * @param tpa IN reference to the thread pipe array
+     */
+    void thread_status_init(struct thread_status_s *ts, int id,
+                            struct thread_pipe_array_s *tpa);
+
+
+    
+    /**
+     * Load the files associated to memory mapped status
+     * @param ts IN/OUT pointer to the thread status structure
+     * @param status_file_prefix IN the prefix used for status files
+     * @return a reason code
+     */
+    int thread_status_load_files(struct thread_status_s *ts,
+                                 const char *status_file_prefix);
+
+    
+
+    /**
+     * Scan the status file and enqueue recovery pending transactions in
+     * the server recovery table
+     * @param ts IN/OUT thread status reference
+     * @param srt IN/OUT server recovery table reference
+     * @return a standardized return code
+     */
+    int thread_status_recovery(struct thread_status_s *ts,
+                               srvr_rcvr_tbl_t *srt);
+
+
+
+    /**
+     * Check a transaction header block and determines if it's in recovery
+     * pending state
+     * @param data IN transaction header block must be analyzed
+     * @return a boolean value
+     */
+    int thread_status_is_recovery_pending(
+        const struct status_record_data_s *data);
+
+
+    
+    /**
+     * Synchronize status files: this is the atomic operation necessary to
+     * guarantee transactionality property of the system
+     * @param ts IN thread status reference
+     * @return a reason code
+     */
+    int thread_status_sync_files(struct thread_status_s *ts);
+
+
+    
+    /**
+     * Remove all records from an updated records tree
+     * @param ur IN/OUT the reference to updated records GTree structure
+     */
+    static inline void thread_status_updated_records_clean(GTree *ur) {
+        LIXA_TRACE(("thread_status_updated_records_clean: cleaning "
+                    "tree allocated at %p\n", ur));
+        g_tree_foreach(ur, traverse_and_delete, ur);
+    }
+
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+
+
+/* restore old value of LIXA_TRACE_MODULE */
+#ifdef LIXA_TRACE_MODULE_SAVE
+# undef LIXA_TRACE_MODULE
+# define LIXA_TRACE_MODULE LIXA_TRACE_MODULE_SAVE
+# undef LIXA_TRACE_MODULE_SAVE
+#endif /* LIXA_TRACE_MODULE_SAVE */
+
+
+
+#endif /* SERVER_THREAD_STATUS_H */
