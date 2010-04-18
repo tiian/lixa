41516a219f7596fa2da09f7fc9de3528fedf4d06
Christian Ferrari <tiian@users.noreply.github.com>
2010-04-18 16:08:57 +0000

    Intermediate commit

--- src/client/lixa_ax.c
+++ src/client/lixa_ax.c
@@ -20,6 +20,8 @@
 
 
 
+#include <client_status.h>
+#include <lixa_errors.h>
 #include <lixa_trace.h>
 #include <xa.h>
 
@@ -34,11 +36,49 @@
 
 
 int ax_reg(int rmid, XID *xid, long flags)
+/*
 {
-    /* @@@ dummy implementation to be fixed: bug 2913849 */
-    LIXA_TRACE(("ax_reg: rmid=%d, xid=%p, flags=0x%lx\n", rmid, xid, flags));
     xid->formatID = -1;
     return TM_OK;
+}
+*/
+{
+    enum Exception { STATUS_NOT_FOUND
+                     , COLL_GET_CS_ERROR
+                     , NONE } excp;
+    int ret_cod = TMER_TMERR;
+    
+    LIXA_TRACE(("ax_reg: rmid=%d, xid=%p, flags=0x%lx\n", rmid, xid, flags));
+    TRY {
+        client_status_t *cs;
+        
+        /* retrieve a reference to the thread status */
+        ret_cod = client_status_coll_get_cs(&global_csc, &cs);
+        switch (ret_cod) {
+            case LIXA_RC_OK: /* nothing to do */
+                break;
+            case LIXA_RC_OBJ_NOT_FOUND:
+                THROW(STATUS_NOT_FOUND);
+            default:
+                THROW(COLL_GET_CS_ERROR);
+        }
+
+        THROW(NONE);
+    } CATCH {
+        switch (excp) {
+            case STATUS_NOT_FOUND:
+                ret_cod = TMER_PROTO;
+                break;
+            case NONE:
+                ret_cod = TM_OK;
+                break;
+            default:
+                ret_cod = TMER_TMERR;
+        } /* switch (excp) */
+    } /* TRY-CATCH */
+    LIXA_TRACE(("/excp=%d/"
+                "ret_cod=%d/errno=%d\n", excp, ret_cod, errno));
+    return ret_cod;
 }
 
 
