f4188a39666b7325382c0e355338a542b0c13d69
tiian <tiian@39947302-e88a-4d6c-85f1-75da6402909b>
2010-06-03 18:56:09 +0000

    Added some missing files for new release 0.5.11

---
+++ tests/src/case0022.c
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2009-2010, Christian Ferrari <tiian@users.sourceforge.net>
+ * All rights reserved.
+ *
+ * This file is part of LIXA.
+ *
+ * LIXA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * LIXA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LIXA.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <config.h>
+
+
+
+#ifdef HAVE_STDIO_H
+# include <stdio.h>
+#endif
+#ifdef HAVE_UNISTD_H
+# include <unistd.h>
+#endif
+#ifdef HAVE_ASSERT_H
+# include <assert.h>
+#endif
+
+
+
+#include <tx.h>
+#include <liblixamonkey.h>
+
+
+/* This case test is for tx_begin() */
+
+
+int main(int argc, char *argv[])
+{
+    char *pgm = argv[0];
+    int rc, test_rc;
+    
+    if (argc < 2) {
+        fprintf(stderr, "%s: at least two options must be specified\n",
+                argv[0]);
+        exit (1);
+    }
+
+    test_rc = strtol(argv[1], NULL, 0);
+
+    printf("%s| starting...\n", pgm);
+
+    printf("%s| tx_open(): %d\n", pgm, rc = tx_open());
+    assert(TX_OK == rc);
+
+    printf("%s| tx_close(): %d\n", pgm, rc = tx_close());
+    assert(test_rc == rc);
+
+    printf("%s| ...finished\n", pgm);
+    return 0;
+}
