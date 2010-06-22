b1c0bf1b97ae3d20db95529bb34c1a3077b2bf1a
Christian Ferrari <tiian@users.noreply.github.com>
2010-06-22 20:32:33 +0000

    Intermediate commit

--- tests/src/case0026.c
+++ tests/src/case0026.c
@@ -53,60 +53,70 @@ int main(int argc, char *argv[])
     
     printf("%s| starting...\n", pgm);
     printf("%s| tx_open(): %d\n", pgm, rc = tx_open());
+    assert(TX_OK == rc);
+    
+    printf("%s| tx_begin(): %d\n", pgm, rc = tx_begin());
     if (0 == fail_point)
         assert(TX_FAIL == rc);
     else
         assert(TX_OK == rc);
     
     printf("%s| tx_set_transaction_control(): %d\n", pgm,
-           rc = tx_set_transaction_control(TX_CHAINED));
+           rc = tx_set_transaction_control(TX_UNCHAINED));
+    if (0 == fail_point)
+        assert(TX_FAIL == rc);
+    else
+        assert(TX_OK == rc);
+
+    printf("%s| tx_set_commit_return(): %d\n", pgm,
+           rc = tx_set_commit_return(TX_COMMIT_COMPLETED));
+    if (0 == fail_point)
+        assert(TX_FAIL == rc);
+    else
+        assert(TX_OK == rc);
+    
+    printf("%s| tx_set_transaction_timeout(): %d\n", pgm,
+           rc = tx_set_transaction_timeout(1));
+    if (0 == fail_point)
+        assert(TX_FAIL == rc);
+    else
+        assert(TX_OK == rc);
+    
+    printf("%s| tx_info(): %d\n", pgm, tx_info(&info));
     if (0 == fail_point) {
         assert(TX_FAIL == rc);
-        exit(0);
+        printf("%s| ...finished (TX_FAIL)\n", pgm);
+        return 0;
     } else
         assert(TX_OK == rc);
     
-    printf("%s| tx_begin(): %d\n", pgm, rc = tx_begin());
-    assert(TX_OK == rc);
-    
-    printf("%s| tx_set_transaction_control(): %d\n", pgm,
-           rc = tx_set_transaction_control(TX_CHAINED));
-    assert(TX_OK == rc);
-    
-    printf("%s| tx_info(): %d\n", pgm, tx_info(&info));
-    assert(TX_CHAINED == info.transaction_control);
-    
-    printf("%s| tx_set_transaction_control(): %d\n", pgm,
-           rc = tx_set_transaction_control(333));
-    assert(TX_EINVAL == rc);
-    
-    printf("%s| tx_info(): %d\n", pgm, tx_info(&info));
-    assert(TX_CHAINED == info.transaction_control);
-    
     printf("%s| tx_commit(): %d\n", pgm, rc = tx_commit());
-    assert(TX_OK == rc);
+    if (1 == fail_point)
+        assert(TX_FAIL == rc);
+    else
+        assert(TX_OK == rc);
     
     printf("%s| tx_begin(): %d\n", pgm, rc = tx_begin());
-    assert(TX_PROTOCOL_ERROR == rc);
+    if (1 == fail_point) {
+        assert(TX_FAIL == rc);
+        printf("%s| ...finished (TX_FAIL)\n", pgm);
+        return 0;
+    } else
+        assert(TX_OK == rc);
     
     printf("%s| tx_rollback(): %d\n", pgm, rc = tx_rollback());
-    assert(TX_OK == rc);
-    
-    printf("%s| tx_begin(): %d\n", pgm, rc = tx_begin());
-    assert(TX_PROTOCOL_ERROR == rc);
-    
-    printf("%s| tx_set_transaction_control(): %d\n", pgm,
-           rc = tx_set_transaction_control(TX_UNCHAINED));
-    assert(TX_OK == rc);
-    
-    printf("%s| tx_info(): %d\n", pgm, tx_info(&info));
-    assert(TX_UNCHAINED == info.transaction_control);
-    
-    printf("%s| tx_commit(): %d\n", pgm, rc = tx_commit());
-    assert(TX_OK == rc);
-    
+    if (2 == fail_point)
+        assert(TX_FAIL == rc);
+    else
+        assert(TX_OK == rc);
+
     printf("%s| tx_close(): %d\n", pgm, rc = tx_close());
-    assert(TX_OK == rc);
+    if (2 == fail_point) {
+        assert(TX_FAIL == rc);
+        printf("%s| ...finished (TX_FAIL)\n", pgm);
+        exit(0);
+    } else
+        assert(TX_OK == rc);
     
     printf("%s| ...finished\n", pgm);
     return 0;
