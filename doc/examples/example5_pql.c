c15c4bba8d33037ede04e3f5da1f62eda6180d2f
tiian <tiian@39947302-e88a-4d6c-85f1-75da6402909b>
2011-05-12 20:41:21 +0000

    PostgreSQL interface implementation started

---
+++ doc/examples/example5_pql.c
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2009-2011, Christian Ferrari <tiian@users.sourceforge.net>
+ * All rights reserved.
+ *
+ * This file is part of LIXA.
+ *
+ * LIXA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * LIXA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LIXA.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <libpq-fe.h>
+
+
+
+static void exit_nicely(PGconn *conn)
+{
+    PQfinish(conn);
+    exit(1);
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    const char *conninfo;
+    PGconn     *conn;
+    PGresult   *res;
+
+    conninfo = "dbname = testdb";
+    
+    conn = PQconnectdb(conninfo);
+    if (CONNECTION_OK != PQstatus(conn)) {
+        fprintf(stderr, "Connection to database failed: %s",
+                PQerrorMessage(conn));
+        exit_nicely(conn);
+    } else
+        printf("conn=%p\n", conn);
+    
+    res = PQexec(conn, "BEGIN");
+    if (PGRES_COMMAND_OK != PQresultStatus(res))
+    {
+        fprintf(stderr, "BEGIN command failed: %s", PQerrorMessage(conn));
+        PQclear(res);
+        exit_nicely(conn);
+    }
+    PQclear(res);
+
+    res = PQexec(conn,
+                 "INSERT INTO authors VALUES(1, 'Ferrari', 'Christian');");
+    if (PGRES_COMMAND_OK != PQresultStatus(res))
+    {
+        fprintf(stderr, "INSERT INTO authors: %s", PQerrorMessage(conn));
+        PQclear(res);
+        exit_nicely(conn);
+    }
+    PQclear(res);
+
+    res = PQexec(conn, "PREPARE TRANSACTION 'foo-bar';");
+    if (PGRES_COMMAND_OK != PQresultStatus(res))
+    {
+        fprintf(stderr, "PREPARE TRANSACTION command failed: %s", PQerrorMessage(conn));
+        PQclear(res);
+        exit_nicely(conn);
+    }
+    PQclear(res);
+/*
+    res = PQexec(conn, "ROLLBACK PREPARED 'foo-bar';");
+    if (PGRES_COMMAND_OK != PQresultStatus(res))
+    {
+        fprintf(stderr, "ROLLBACK PREPARED command failed: %s", PQerrorMessage(conn));
+        PQclear(res);
+        exit_nicely(conn);
+    }
+    PQclear(res);
+*/
+
+    res = PQexec(conn, "COMMIT PREPARED 'foo-bar';");
+    if (PGRES_COMMAND_OK != PQresultStatus(res))
+    {
+        fprintf(stderr, "COMMIT PREPARED command failed: %s", PQerrorMessage(conn));
+        PQclear(res);
+        exit_nicely(conn);
+    }
+    PQclear(res);
+
+    PQfinish(conn);
+
+    return 0;
+}
