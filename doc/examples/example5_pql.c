e9fd0c519e28821110ee164d83fff92d36beb19c
Christian Ferrari <tiian@users.noreply.github.com>
2011-09-14 20:01:34 +0000

    Documentation only commit.

--- doc/examples/example5_pql.c
+++ doc/examples/example5_pql.c
@@ -18,6 +18,7 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* PostgreSQL */
 #include <libpq-fe.h>
@@ -41,14 +42,19 @@ static void exit_nicely(PGconn *conn)
 int main(int argc, char *argv[])
 {
     /* generic variables */
-    int         txrc;
+    int         txrc, delete;
     /* PostgreSQL variables */
     const char *conninfo;
     PGconn     *conn;
     PGresult   *res;
 
     conninfo = "dbname = testdb";
-    
+
+    if (argc > 1 && (!strcmp(argv[1], "delete") || !strcmp(argv[1], "DELETE")))
+        delete = 1;
+    else
+        delete = 0;
+ 
     /* open the resource manager(s) */
     if (TX_OK != (txrc = tx_open())) {
         fprintf(stderr, "tx_open error: %d\n", txrc);
@@ -57,93 +63,61 @@ int main(int argc, char *argv[])
 
     conn = lixa_pq_get_conn();
     /*
-    conn = lixa_pq_get_conn_by_pos(0);
-    conn = lixa_pq_get_conn_by_rmid(0);
-    */
+     * These functions can be used when there are more than one PostgreSQL
+     * configured as a resource manager
+     * conn = lixa_pq_get_conn_by_pos(0);
+     * conn = lixa_pq_get_conn_by_rmid(0);
+     */
 
-    /* replaced by tx_open()
-    conn = PQconnectdb(conninfo);
-    if (CONNECTION_OK != PQstatus(conn)) {
-        fprintf(stderr, "Connection to database failed: %s",
-                PQerrorMessage(conn));
-        exit_nicely(conn);
-    }
-    */
-    
     /* start a new transaction */
     if (TX_OK != (txrc = tx_begin())) {
         fprintf(stderr, "tx_begin error: %d\n", txrc);
         exit(txrc);
     }
-    /* replaced by tx_begin()
-    res = PQexec(conn, "BEGIN");
-    if (PGRES_COMMAND_OK != PQresultStatus(res))
-    {
-        fprintf(stderr, "BEGIN command failed: %s", PQerrorMessage(conn));
-        PQclear(res);
-        exit_nicely(conn);
-    }
-    PQclear(res);
-    */ 
 
-    res = PQexec(conn,
-                 "INSERT INTO authors VALUES(1, 'Ferrari', 'Christian');");
-    if (PGRES_COMMAND_OK != PQresultStatus(res))
-    {
-        fprintf(stderr, "INSERT INTO authors: %s", PQerrorMessage(conn));
+    if (delete) {
+        printf("Deleting a row from the table...\n");
+        res = PQexec(conn,
+                     "DELETE FROM authors WHERE id=1;");
+        if (PGRES_COMMAND_OK != PQresultStatus(res))
+            {
+                fprintf(stderr, "DELETE FROM authors: %s",
+                        PQerrorMessage(conn));
+                PQclear(res);
+                exit_nicely(conn);
+            }
         PQclear(res);
-        exit_nicely(conn);
-    }
-    PQclear(res);
-/*
-    res = PQexec(conn, "PREPARE TRANSACTION 'foo-bar';");
-    if (PGRES_COMMAND_OK != PQresultStatus(res))
-    {
-        fprintf(stderr, "PREPARE TRANSACTION command failed: %s", PQerrorMessage(conn));
+    } else {
+        printf("Inserting a row in the table...\n");
+        res = PQexec(conn,
+                     "INSERT INTO authors VALUES(1, 'Foo', 'Bar');");
+        if (PGRES_COMMAND_OK != PQresultStatus(res))
+            {
+                fprintf(stderr, "INSERT INTO authors: %s",
+                        PQerrorMessage(conn));
+                PQclear(res);
+                exit_nicely(conn);
+            }
         PQclear(res);
-        exit_nicely(conn);
     }
-    PQclear(res);
-*/
-/*
-    res = PQexec(conn, "ROLLBACK PREPARED 'foo-bar';");
-    if (PGRES_COMMAND_OK != PQresultStatus(res))
-    {
-        fprintf(stderr, "ROLLBACK PREPARED command failed: %s", PQerrorMessage(conn));
-        PQclear(res);
-        exit_nicely(conn);
+    
+    if (TX_OK != (txrc = tx_commit())) {
+        fprintf(stderr, "tx_commit error: %d\n", txrc);
+        exit(txrc);
     }
-    PQclear(res);
-*/
 /*
-    res = PQexec(conn, "COMMIT PREPARED 'foo-bar';");
-    if (PGRES_COMMAND_OK != PQresultStatus(res))
-    {
-        fprintf(stderr, "COMMIT PREPARED command failed: %s", PQerrorMessage(conn));
-        PQclear(res);
-        exit_nicely(conn);
-    }
-    PQclear(res);
-*/
-/*
+ * This function can be used if you want to rollback instead of commit the
+ * transaction
     if (TX_OK != (txrc = tx_rollback())) {
         fprintf(stderr, "tx_rollback error: %d\n", txrc);
         exit(txrc);
     }
 */
-    if (TX_OK != (txrc = tx_commit())) {
-        fprintf(stderr, "tx_commit error: %d\n", txrc);
-        exit(txrc);
-    }
 
     if (TX_OK != (txrc = tx_close())) {
         fprintf(stderr, "tx_close error: %d\n", txrc);
         exit(txrc);
     }
 
-    /*
-    PQfinish(conn);
-    */
-    
     return 0;
 }
