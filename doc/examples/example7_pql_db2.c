6cea0d6da5d29c74b40fe66d8ecb62218ec265c9
Christian Ferrari <tiian@users.noreply.github.com>
2011-10-13 20:53:49 +0000

    Implementing MySQL interface

--- doc/examples/example7_pql_db2.c
+++ doc/examples/example7_pql_db2.c
@@ -114,7 +114,6 @@ int main(int argc, char *argv[])
     /*
      * These functions can be used when there are more than one PostgreSQL
      * configured as a resource manager
-     * pq_conn = lixa_pq_get_conn_by_pos(0);
      * pq_conn = lixa_pq_get_conn_by_rmid(0);
      */
 
---
+++ src/client/switch/mysql/mysql_stareg.c
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2009-2011, Christian Ferrari <tiian@users.sourceforge.net>
+ * All rights reserved.
+ *
+ * This file is part of LIXA.
+ *
+ * LIXA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * LIXA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LIXA.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+#include "xa.h"
+
+
+
+/*
+ * This is an external implementation of a partial XA interface for
+ * MySQL.
+ * The intent of this piece of software is allowing user to start using
+ * LIXA with any recent version of MySQL.
+ * A definitive XA interface should be implemented inside MySQL
+ * client library.
+ * Wrapping the standard interface, only the static registration can be
+ * implemented.
+ */
+
+
+
+#include <lixa_trace.h>
+#include <lixa_common_status.h>
+#include <liblixapq.h>
+
+
+
+/* set module trace flag */
+#ifdef LIXA_TRACE_MODULE
+# undef LIXA_TRACE_MODULE
+#endif /* LIXA_TRACE_MODULE */
+#define LIXA_TRACE_MODULE   LIXA_TRACE_MOD_CLIENT_XA
+
+
+
+/*
+ * This is MySQL specific: the xa_switch_t struct supplied is named xapqls
+ * for XA static registration
+ */
+extern struct xa_switch_t xapqls;
+
+
+
+/*
+ * The function is exported and dynamically retrieved after the module was
+ * fetched
+ */
+struct xa_switch_t *lixa_get_xa_switch()
+{
+    return &xapqls;
+}
