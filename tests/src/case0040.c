1e03b037a81b02e7c2e3ba37d1f2d9bfd110f7fe
tiian <tiian@39947302-e88a-4d6c-85f1-75da6402909b>
2011-10-19 20:53:06 +0000

    .

---
+++ tests/src/case0040.c
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2009-2011, Christian Ferrari <tiian@users.sourceforge.net>
+ * All rights reserved.
+ *
+ * This file is part of LIXA.
+ *
+ * LIXA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * LIXA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LIXA.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <config.h>
+
+
+
+#ifdef HAVE_STDIO_H
+# include <stdio.h>
+#endif
+#ifdef HAVE_UNISTD_H
+# include <unistd.h>
+#endif
+#ifdef HAVE_ASSERT_H
+# include <assert.h>
+#endif
+
+
+
+#include <tx.h>
+
+
+
+/* this is a special case test: it is used to test the parsing function
+   in liblixamy */
+
+
+
+int main(int argc, char *argv[])
+{
+    char *pgm = argv[0];
+    int rc;
+    
+    printf("%s| starting...\n", pgm);
+    printf("%s| tx_open(): %d\n", pgm, rc = tx_open());
+    assert(TX_OK == rc);
+    printf("%s| tx_close(): %d\n", pgm, rc = tx_close());
+    assert(TX_OK == rc);
+    printf("%s| ...finished\n", pgm);
+    return 0;
+}
