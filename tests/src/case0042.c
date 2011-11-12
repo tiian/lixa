97246cbe5c95cd94a98ec16cc0b7c33af0e8bffd
Christian Ferrari <tiian@users.noreply.github.com>
2011-11-12 11:40:39 +0000

    Test only revision

--- tests/src/case0042.c
+++ tests/src/case0042.c
@@ -22,17 +22,24 @@
 /* PostgreSQL */
 #include <libpq-fe.h>
 
+/* MySQL */
+#include <mysql.h>
+
 /* TX (Transaction Demarcation) header */
 #include <tx.h>
 
 /* LIXA help library for PostgreSQL */
 #include <lixapq.h>
 
+/* LIXA help library for MySQL */
+#include <lixamy.h>
 
 
-static void exit_nicely(PGconn *conn)
+
+void exit_nicely(PGconn *pq_conn, MYSQL *my_conn)
 {
-    PQfinish(conn);
+    PQfinish(pq_conn);
+    mysql_close(my_conn);
     exit(1);
 }
 
@@ -43,8 +50,10 @@ int main(int argc, char *argv[])
     /* generic variables */
     int         txrc;
     /* PostgreSQL variables */
-    PGconn     *conn;
+    PGconn     *pq_conn;
     PGresult   *res;
+    /* MySQL variables */
+    MYSQL      *my_conn;
     /* control variables */
     int         commit;
     int         insert;
@@ -66,133 +75,15 @@ int main(int argc, char *argv[])
     }
 
     /* retrieve the connection handler */
-    conn = lixa_pq_get_conn();
-    if (NULL == conn) {
+    pq_conn = lixa_pq_get_conn();
+    if (NULL == pq_conn) {
         fprintf(stderr, "lixa_pq_get_conn: conn is NULL\n");
         exit(1);
     }
     
-    /* start a new transaction */
-    if (TX_OK != (txrc = tx_begin())) {
-        fprintf(stderr, "tx_begin error: %d\n", txrc);
-        exit(txrc);
-    }
-
-    /* insert data */
-    if (insert) {
-        res = PQexec(
-            conn, "INSERT INTO authors VALUES(9, 'Name', 'Surname');");
-        if (PGRES_COMMAND_OK != PQresultStatus(res)) {
-            fprintf(stderr,
-                    "INSERT INTO authors: %s", PQerrorMessage(conn));
-            PQclear(res);
-            exit_nicely(conn);
-        }
-        PQclear(res);
-    } else {
-        res = PQexec(
-            conn, "DELETE FROM authors;");
-        if (PGRES_COMMAND_OK != PQresultStatus(res)) {
-            fprintf(stderr,
-                    "DELETE FROM authors: %s", PQerrorMessage(conn));
-            PQclear(res);
-            exit_nicely(conn);
-        }
-        PQclear(res);
-    }
-    
-    /* commit transaction */
-    if (commit) {
-        if (test_rc != (txrc = tx_commit())) {
-            fprintf(stderr, "tx_commit error: %d instead of %d\n",
-                    txrc, test_rc);
-            exit(txrc);
-        }
-    } else {
-        if (test_rc != (txrc = tx_rollback())) {
-            fprintf(stderr, "tx_rollback error: %d instead of %d\n",
-                    txrc, test_rc);
-            exit(txrc);
-        }
-    }
-
-    /* close the resource manager */
-    if (TX_OK != (txrc = tx_close())) {
-        fprintf(stderr, "tx_close error: %d\n", txrc);
-        exit(txrc);
-    }
-
-    return 0;
-}
-/*
- * Copyright (c) 2009-2011, Christian Ferrari <tiian@users.sourceforge.net>
- * All rights reserved.
- *
- * This file is part of LIXA.
- *
- * LIXA is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as published
- * by the Free Software Foundation.
- *
- * LIXA is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with LIXA.  If not, see <http://www.gnu.org/licenses/>.
- */
-#include <stdio.h>
-#include <stdlib.h>
-
-/* MySQL */
-#include <mysql.h>
-
-/* TX (Transaction Demarcation) header */
-#include <tx.h>
-
-/* LIXA help library for MySQL */
-#include <lixamy.h>
-
-
-
-static void exit_nicely(MYSQL *conn)
-{
-    mysql_close(conn);
-    exit(1);
-}
-
-
-
-int main(int argc, char *argv[])
-{
-    /* generic variables */
-    int         txrc;
-    /* MySQL variables */
-    MYSQL      *conn;
-    /* control variables */
-    int         commit;
-    int         insert;
-    int         test_rc;
-    
-    if (argc < 4) {
-        fprintf(stderr, "%s: at least two options must be specified\n",
-                argv[0]);
-        exit (1);
-    }
-    commit = strtol(argv[1], NULL, 0);
-    insert = strtol(argv[2], NULL, 0);
-    test_rc = strtol(argv[3], NULL, 0);
-
-    /* open the resource manager(s) */
-    if (TX_OK != (txrc = tx_open())) {
-        fprintf(stderr, "tx_open error: %d\n", txrc);
-        exit(txrc);
-    }
-
     /* retrieve the connection handler */
-    conn = lixa_my_get_conn();
-    if (NULL == conn) {
+    my_conn = lixa_my_get_conn();
+    if (NULL == my_conn) {
         fprintf(stderr, "lixa_my_get_conn: conn is NULL\n");
         exit(1);
     }
@@ -205,17 +96,35 @@ int main(int argc, char *argv[])
 
     /* insert data */
     if (insert) {
-        if (mysql_query(conn,
+        res = PQexec(
+            pq_conn, "INSERT INTO authors VALUES(9, 'Name', 'Surname');");
+        if (PGRES_COMMAND_OK != PQresultStatus(res)) {
+            fprintf(stderr,
+                    "INSERT INTO authors: %s", PQerrorMessage(pq_conn));
+            PQclear(res);
+            exit_nicely(pq_conn, my_conn);
+        }
+        PQclear(res);
+        if (mysql_query(my_conn,
                         "INSERT INTO authors VALUES(9, 'Name', 'Surname')")) {
             fprintf(stderr, "INSERT INTO authors: %u/%s",
-                    mysql_errno(conn), mysql_error(conn));
-            exit_nicely(conn);
+                    mysql_errno(my_conn), mysql_error(my_conn));
+            exit_nicely(pq_conn, my_conn);
         }
     } else {
-        if (mysql_query(conn, "DELETE FROM authors")) {
+        res = PQexec(
+            pq_conn, "DELETE FROM authors;");
+        if (PGRES_COMMAND_OK != PQresultStatus(res)) {
+            fprintf(stderr,
+                    "DELETE FROM authors: %s", PQerrorMessage(pq_conn));
+            PQclear(res);
+            exit_nicely(pq_conn, my_conn);
+        }
+        PQclear(res);
+        if (mysql_query(my_conn, "DELETE FROM authors")) {
             fprintf(stderr, "DELETE FROM  authors: %u/%s",
-                    mysql_errno(conn), mysql_error(conn));
-            exit_nicely(conn);
+                    mysql_errno(my_conn), mysql_error(my_conn));
+            exit_nicely(pq_conn, my_conn);
         }
     }
     
@@ -242,3 +151,5 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+
+
