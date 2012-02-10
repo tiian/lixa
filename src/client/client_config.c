4bbb20b3c30e3fe0e08dca14951e8b55858a0c5a
tiian <tiian@39947302-e88a-4d6c-85f1-75da6402909b>
2012-02-10 22:41:30 +0000

    Work in progress for Python interface

--- src/client/client_config.c
+++ src/client/client_config.c
@@ -538,7 +538,7 @@ int client_config_load_switch(const client_config_coll_t *ccc)
             
             if (NULL == (module = g_module_open(
                              (gchar *)act_rsrmgr->generic->switch_file,
-                             G_MODULE_BIND_LOCAL))) {
+                             G_MODULE_BIND_LOCAL | G_MODULE_BIND_LAZY))) {
                 LIXA_TRACE(("client_config_load_switch: switch_file='%s', "
                             "g_module_error='%s'\n",
                             act_rsrmgr->generic->switch_file,
