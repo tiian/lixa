dce11fc848b27881a4436b48773d90cc05bc52a4
tiian <tiian@39947302-e88a-4d6c-85f1-75da6402909b>
2012-04-06 15:40:16 +0000

    Documentation commit

--- ext/php/examples/example01_lixa.php
+++ ext/php/examples/example01_lixa.php
@@ -1,21 +1,23 @@
 <?php	
-	/* set PHPRC to /home/tiian/src/swig to pick-up php.ini when using
-	   /opt/php/bin/php to run this script; it's not necessary if using
-	   /opt/php/bin/php-cgi */
-	/* set LIXA_PROFILE=MYS_STA_PQL_STA */
-	/* run me using /opt/php/bin/php -c php.ini foo.php */
-	/*
-	include("lixa.php");
-	*/
-	print "Hello world!\n";
+	print "Trying to open all the Resource Managers with tx_open()...\n";
 	$rc=tx_open();
-	print "tx_open() --> $rc\n"; 
+	if (TX_OK != $rc) 
+		exit("tx_open returned rc=$rc, can't going on!\n");
+	
+	print "Trying to start a new transaction with tx_begin()...\n";	
 	$rc=tx_begin();
-	print "tx_begin() --> $rc\n"; 
+	if (TX_OK != $rc) 
+		exit("tx_begin returned rc=$rc, can't going on!\n");
+
+	print "Trying to commit the transaction with tx_commit()...\n";	
 	$rc=tx_commit();
-	print "tx_commit() --> $rc\n"; 
+	if (TX_OK != $rc) 
+		exit("tx_commit returned rc=$rc, can't going on!\n");
+
+	print "Trying to close all the Resource Managers with tx_close()...\n";
 	$rc=tx_close();
-	print "tx_close() --> $rc\n"; 
-	$rc=TX_OK;
-	print "TX_OK=$rc\n";
+	if (TX_OK != $rc) 
+		exit("tx_close returned rc=$rc, can't going on!\n");
+
+	print "Execution completed!\n";
 ?>
