ccd91fd16d2923e13df325866cf15c1d363212fb
Christian Ferrari <tiian@users.noreply.github.com>
2012-04-11 20:30:10 +0000

    Documentation for LIXA and 1 MySQL (PHP language) completed.

--- ext/php/examples/example02_mysqli.php
+++ ext/php/examples/example02_mysqli.php
@@ -1,22 +1,22 @@
 <?php
-echo "Using mysqli direct API\n";
-$mysqli = new mysqli("localhost", "lixa", "", "lixa", 3306);
-if ($mysqli->connect_errno)
-	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	/* retrieve MySQL connection */
+	$mysqli = new mysqli("localhost", "lixa", "", "lixa", 3306);
+	if ($mysqli->connect_errno)
+		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 
-if (!$mysqli->query("DELETE FROM authors;"))
-	echo "DELETE failed: (" . $mysqli->errno . ") " . $mysqli->error;
-if (!$mysqli->query("INSERT INTO authors VALUES(999,'surname','name');"))
-	echo "INSERT failed: (" . $mysqli->errno . ") " . $mysqli->error;
-if (!$mysqli->query("INSERT INTO authors VALUES(969,'Ferrari','Christian');"))
-	echo "INSERT failed: (" . $mysqli->errno . ") " . $mysqli->error;
-if (!$mysqli->query("INSERT INTO authors VALUES(333,'Ferrari','C');"))
-	echo "INSERT failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	/* remove all the rows from the table */
+	if (!$mysqli->query("DELETE FROM authors;"))
+		echo "DELETE failed: (" . $mysqli->errno . ") " . $mysqli->error;
 
-$mysqli->real_query("SELECT id,last_name,first_name FROM authors ORDER BY id DESC;");
-$res = $mysqli->use_result();
-echo "Result set order...\n";
-while ($row = $res->fetch_assoc())
-	echo "id=" . $row['id'] . ", last_name=" . $row['last_name'] . ", first_name=" . $row['first_name'] . "\n";
+	/* populate the table */
+	if (!$mysqli->query("INSERT INTO authors VALUES(969,'Ferrari','Christian');"))
+		echo "INSERT failed: (" . $mysqli->errno . ") " . $mysqli->error;
+
+	/* check the content of the table */
+	$mysqli->real_query("SELECT id,last_name,first_name FROM authors ORDER BY id DESC;");
+	$res = $mysqli->use_result();
+	echo "Result set:\n";
+	while ($row = $res->fetch_assoc())
+		echo "id=" . $row['id'] . ", last_name=" . $row['last_name'] . ", first_name=" . $row['first_name'] . "\n";
 ?>
 
