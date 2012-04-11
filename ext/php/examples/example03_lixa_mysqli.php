dc0d729217745542ce9116aa847da68265274862
tiian <tiian@39947302-e88a-4d6c-85f1-75da6402909b>
2012-04-11 20:30:10 +0000

    Documentation for LIXA and 1 MySQL (PHP language) completed.

--- ext/php/examples/example03_lixa_mysqli.php
+++ ext/php/examples/example03_lixa_mysqli.php
@@ -1,97 +1,102 @@
 <?php	
-	/* set PHPRC to /home/tiian/src/swig to pick-up php.ini when using
-	   /opt/php/bin/php to run this script; it's not necessary if using
-	   /opt/php/bin/php-cgi */
-	/* set LIXA_PROFILE=MYS_STA_PQL_STA */
-	/* run me using /opt/php/bin/php -c php.ini foo.php */
-	/*
-	include("lixa.php");
-	*/
+	/* open all the Resource Managers (MySQL instance in this example) */
 	$rc=tx_open();
-	print "tx_open() --> $rc\n"; 
-	
+	print "tx_open(): $rc\n"; 
+
+	/* retrieve MySQL connection from LIXA Transaction Manager instead
+	   of MySQL directly: lixa// means:
+	   database name=lixa
+	   chooser type (pos/rmid)=none
+	   id (0, 1, 2, ...)=none
+	   and can be read as "retrieve the first available MySQL connection
+	   established by the Transaction Manager using the current
+	   LIXA profile */
 	$mysqli = new mysqli("localhost", "lixa", "", "lixa//", 3306);
 	if ($mysqli->connect_errno)
 		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error . "\n";
 
+	/* start a new transaction coordinated by LIXA */
 	$rc=tx_begin();
-	print "tx_begin() --> $rc\n"; 
+	print "tx_begin(): $rc\n"; 
 
+	/* remove all the rows from the table */
 	print "DELETE FROM authors;\n";
 	if (!$mysqli->query("DELETE FROM authors;"))
 		echo "DELETE failed: (" . $mysqli->errno . ") " . $mysqli->error;
+
+	/* check the content of the table */	
 	$mysqli->real_query("SELECT id,last_name,first_name FROM authors ORDER BY id DESC;");
 	$res = $mysqli->use_result();
-	echo "Result set order...\n";
+	echo "Result set:\n";
 	while ($row = $res->fetch_assoc())
 		echo "id=" . $row['id'] . ", last_name=" . $row['last_name'] . ", first_name=" . $row['first_name'] . "\n";
 
+	/* commit the transaction (DELETE FROM ...) */
 	$rc=tx_commit();
-	print "tx_commit() --> $rc\n"; 
+	print "tx_commit(): $rc\n"; 
 
+	/* start a new transaction coordinated by LIXA */
 	$rc=tx_begin();
-	print "tx_begin() --> $rc\n"; 
+	print "tx_begin(): $rc\n"; 
 
+	/* check the content of the table */	
 	$mysqli->real_query("SELECT id,last_name,first_name FROM authors ORDER BY id DESC;");
 	$res = $mysqli->use_result();
-	echo "Result set order...\n";
+	echo "Result set:\n";
 	while ($row = $res->fetch_assoc())
 		echo "id=" . $row['id'] . ", last_name=" . $row['last_name'] . ", first_name=" . $row['first_name'] . "\n";
 
-	print "INSERT INTO authors...\n";
-	if (!$mysqli->query("INSERT INTO authors VALUES(999,'surname','name');"))
-		echo "INSERT failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	/* populate the table */
 	print "INSERT INTO authors...\n";
 	if (!$mysqli->query("INSERT INTO authors VALUES(969,'Ferrari','Christian');"))
-		echo "INSERT failed: (" . $mysqli->errno . ") " . $mysqli->error;
-	print "INSERT INTO authors...\n";
-	if (!$mysqli->query("INSERT INTO authors VALUES(333,'Ferrari','C');"))
-		echo "INSERT failed: (" . $mysqli->errno . ") " . $mysqli->error;
-	$mysqli->real_query("SELECT id,last_name,first_name FROM authors ORDER BY id DESC;");
-	$res = $mysqli->use_result();
-	echo "Result set order...\n";
-	while ($row = $res->fetch_assoc())
-		echo "id=" . $row['id'] . ", last_name=" . $row['last_name'] . ", first_name=" . $row['first_name'] . "\n";
-
-	$rc=tx_rollback();
-	print "tx_rollback() --> $rc\n"; 
-
-	$mysqli->real_query("SELECT id,last_name,first_name FROM authors ORDER BY id DESC;");
-	$res = $mysqli->use_result();
-	echo "Result set order...\n";
-	while ($row = $res->fetch_assoc())
-		echo "id=" . $row['id'] . ", last_name=" . $row['last_name'] . ", first_name=" . $row['first_name'] . "\n";
-
-	$rc=tx_begin();
-	print "tx_begin() --> $rc\n"; 
-
-	print "INSERT INTO authors...\n";
-	if (!$mysqli->query("INSERT INTO authors VALUES(999,'surname','name');"))
-		echo "INSERT failed: (" . $mysqli->errno . ") " . $mysqli->error;
-	print "INSERT INTO authors...\n";
-	if (!$mysqli->query("INSERT INTO authors VALUES(969,'Ferrari','Christian');"))
-		echo "INSERT failed: (" . $mysqli->errno . ") " . $mysqli->error;
-	print "INSERT INTO authors...\n";
-	if (!$mysqli->query("INSERT INTO authors VALUES(333,'Ferrari','C');"))
 		echo "INSERT failed: (" . $mysqli->errno . ") " . $mysqli->error;
 	
+	/* check the content of the table */
 	$mysqli->real_query("SELECT id,last_name,first_name FROM authors ORDER BY id DESC;");
 	$res = $mysqli->use_result();
-	echo "Result set order...\n";
+	echo "Result set:\n";
 	while ($row = $res->fetch_assoc())
 		echo "id=" . $row['id'] . ", last_name=" . $row['last_name'] . ", first_name=" . $row['first_name'] . "\n";
 
-	$rc=tx_commit();
-	print "tx_commit() --> $rc\n"; 
+	/* rollback the transaction (INSERT INTO ...) */
+	$rc=tx_rollback();
+	print "tx_rollback(): $rc\n"; 
 
+	/* check the content of the table */
 	$mysqli->real_query("SELECT id,last_name,first_name FROM authors ORDER BY id DESC;");
 	$res = $mysqli->use_result();
-	echo "Result set order...\n";
+	echo "Result set:\n";
+	while ($row = $res->fetch_assoc())
+		echo "id=" . $row['id'] . ", last_name=" . $row['last_name'] . ", first_name=" . $row['first_name'] . "\n";
+
+	/* start a new transaction coordinated by LIXA */
+	$rc=tx_begin();
+	print "tx_begin(): $rc\n"; 
+
+	/* populate the table */
+	print "INSERT INTO authors...\n";
+	if (!$mysqli->query("INSERT INTO authors VALUES(969,'Ferrari','Christian');"))
+		echo "INSERT failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	
+	/* check the content of the table */
+	$mysqli->real_query("SELECT id,last_name,first_name FROM authors ORDER BY id DESC;");
+	$res = $mysqli->use_result();
+	echo "Result set:\n";
+	while ($row = $res->fetch_assoc())
+		echo "id=" . $row['id'] . ", last_name=" . $row['last_name'] . ", first_name=" . $row['first_name'] . "\n";
+
+	/* commit the transaction (INSERT INTO ...) */
+	$rc=tx_commit();
+	print "tx_commit(): $rc\n"; 
+
+	/* check the content of the table */
+	$mysqli->real_query("SELECT id,last_name,first_name FROM authors ORDER BY id DESC;");
+	$res = $mysqli->use_result();
+	echo "Result set:\n";
 	while ($row = $res->fetch_assoc())
 
+	/* close all the Resource Managers */
 	$rc=tx_close();
-	print "tx_close() --> $rc\n"; 
-	$rc=TX_OK;
-	print "TX_OK=$rc\n";
+	print "tx_close(): $rc\n"; 
 ?>
 
