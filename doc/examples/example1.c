4eb83d5791c90968900c7a1d3d0a47c0f73ecd4d
Christian Ferrari <tiian@users.sourceforge.net>
2016-12-16 23:43:14 +0100

    Alignment of example1.c to EXAMPLE1.cbl

--- doc/examples/example1.c
+++ doc/examples/example1.c
@@ -26,19 +26,68 @@
 /* this basic example shows simple usage of the LIXA software */
 
 
+void info(void)
+{
+    int rc;
+    TXINFO info;
+
+    printf("tx_info(): %d\n", rc = tx_info(&info));
+    if (TX_OK > rc) exit(1);
+    printf("xid.formatID: %ld\n", info.xid.formatID);
+    printf("xid.gtrid_length: %ld\n", info.xid.gtrid_length);
+    printf("xid.bqual_length: %ld\n", info.xid.bqual_length);
+    printf("when_return: %ld (%s)\n", info.when_return,
+           TX_COMMIT_COMPLETED == info.when_return ?
+           "TX_COMMIT_COMPLETED" : "TX_COMMIT_DECISION_LOGGED");
+    printf("transaction_control: %ld (%s)\n", info.transaction_control,
+           TX_UNCHAINED == info.transaction_control ?
+           "TX_UNCHAINED" : "TX_CHAINED");
+    printf("transaction_timeout: %ld s\n", info.transaction_timeout);
+    printf("transaction_state: %ld (%s)\n", info.transaction_state,
+           TX_ACTIVE == info.transaction_state ?
+           "TX_ACTIVE" :
+           TX_TIMEOUT_ROLLBACK_ONLY == info.transaction_state ?
+           "TX_TIMEOUT_ROLLBACK_ONLY" : "TX_ROLLBACK_ONLY"
+           );           
+}
+
+
 
 int main(int argc, char *argv[])
 {
     int rc;
-    
+
+    printf("Executing %s\n", argv[0]);
     printf("tx_open(): %d\n", rc = tx_open());
     if (TX_OK != rc) exit(1);
-    
     printf("tx_begin(): %d\n", rc = tx_begin());
     if (TX_OK != rc) exit(1);
+    info();
+    
     printf("tx_commit(): %d\n", rc = tx_commit());
     if (TX_OK != rc) exit(1);
+    info();
 
+    /* set non default parameters */
+    /* LIXA does not support this option */
+    printf("tx_set_commit_return(): %d\n",
+           rc = tx_set_commit_return(TX_COMMIT_DECISION_LOGGED));
+    if (TX_NOT_SUPPORTED != rc) exit(1);
+    /* LIXA supports timeout */
+    printf("tx_set_transaction_timeout(): %d\n",
+           rc = tx_set_transaction_timeout(5));
+    if (TX_OK != rc) exit(1);
+    /* LIXA supports transaction control */
+    printf("tx_set_transaction_control(): %d\n",
+           rc = tx_set_transaction_control(TX_CHAINED));
+    if (TX_OK != rc) exit(1);
+    info();
+    /* Transaction control must be resetted to avoid error during close */
+    printf("tx_set_transaction_control(): %d\n",
+           rc = tx_set_transaction_control(TX_UNCHAINED));
+    if (TX_OK != rc) exit(1);
+    info();
+    
     printf("tx_begin(): %d\n", rc = tx_begin());
     if (TX_OK != rc) exit(1);
     printf("tx_rollback(): %d\n", rc = tx_rollback());
