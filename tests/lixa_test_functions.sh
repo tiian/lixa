e96f73b03201927ad6f5f3d850dcc9ed53084bc8
Pieter Janse van Rensburg <pieter.jvrensburg@globetom.com>
2017-01-24 07:53:09 +0200

    Fix for macOS

--- tests/lixa_test_functions.sh
+++ tests/lixa_test_functions.sh
@@ -105,7 +105,7 @@ start_server() {
 		$SERVER_DIR/lixad --daemon --config-file=$TESTS_ETC_DIR/${LIXAD_CONF_XML} --trace-file=$PWD/lixad.trace 2>>$PWD/lixad.trace
 	fi
 	# wait until the server opens the listening socket
-	while test $(netstat -nta | grep LISTEN | grep 127.0.0.1:2345 | wc -l) -le 0
+	while test $(netstat -nta | grep LISTEN | grep 127.0.0.1 | grep 2345 | wc -l) -le 0
 	do
 		echo "Waiting server socket listening..."
 		sleep 1
