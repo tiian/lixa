46bc0e0dba3ed47b4e82578f6dfc5053578ee6c6
Pieter Janse van Rensburg <pieter.jvrensburg@globetom.com>
2017-01-24 22:36:46 +0200

    First attempt in adding XA tests covering suspended transactions

--- tests/src/case0048.c
+++ tests/src/case0048.c
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with LIXA.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include t<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include <tx.h>
