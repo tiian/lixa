668bb870ece3b7b7448d24b0286e660c755e830d
Christian Ferrari <tiian@users.sourceforge.net>
2017-04-05 23:10:11 +0200

    Started to define MySQLXAResource interface for C

---
+++ src/xta/xta_mysql_xa_resource.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2009-2017, Christian Ferrari <tiian@users.sourceforge.net>
+ * All rights reserved.
+ *
+ * This file is part of LIXA.
+ *
+ * LIXA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * LIXA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LIXA.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <config.h>
+
+
+
+/* LIXA includes */
+#include "lixa_errors.h"
+#include "lixa_trace.h"
+/* XTA includes */
+#include "xta_mysql_xa_resource.h"
+
+
+
+/* set module trace flag */
+#ifdef LIXA_TRACE_MODULE
+# undef LIXA_TRACE_MODULE
+#endif /* LIXA_TRACE_MODULE */
+#define LIXA_TRACE_MODULE   LIXA_TRACE_MOD_XTA
+
+
+
+xta_mysql_xa_resource_t *xta_mysql_xa_resource_new(MYSQL *connection)
+{
+    enum Exception { NONE } excp;
+    int ret_cod = LIXA_RC_INTERNAL_ERROR;
+    xta_mysql_xa_resource_t *xa_res = NULL;
+    
+    LIXA_TRACE(("xta_mysql_xa_resource_new\n"));
+    TRY {
+        
+        THROW(NONE);
+    } CATCH {
+        switch (excp) {
+            case NONE:
+                ret_cod = LIXA_RC_OK;
+                break;
+            default:
+                ret_cod = LIXA_RC_INTERNAL_ERROR;
+        } /* switch (excp) */
+    } /* TRY-CATCH */
+    LIXA_TRACE(("xta_mysql_xa_resource_new/excp=%d/"
+                "ret_cod=%d/errno=%d\n", excp, ret_cod, errno));
+    return xa_res;
+}
+
---
+++ src/xta/xta_mysql_xa_resource.h
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2009-2017, Christian Ferrari <tiian@users.sourceforge.net>
+ * All rights reserved.
+ *
+ * This file is part of LIXA.
+ *
+ * LIXA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * LIXA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LIXA.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef MYSQL_XA_RESOURCE_H
+# define MYSQL_XA_RESOURCE_H
+
+
+
+#include <config.h>
+
+
+/* MySQL front-end */
+#include <mysql.h>
+/* XTA includes */
+#include "xta_xa_resource.h"
+
+
+
+/* save old LIXA_TRACE_MODULE and set a new value */
+#ifdef LIXA_TRACE_MODULE
+# define LIXA_TRACE_MODULE_SAVE LIXA_TRACE_MODULE
+# undef LIXA_TRACE_MODULE
+#else
+# undef LIXA_TRACE_MODULE_SAVE
+#endif /* LIXA_TRACE_MODULE */
+#define LIXA_TRACE_MODULE      LIXA_TRACE_MOD_XTA
+
+
+
+/**
+ * XTA MySQL XA Resource data type
+ */
+typedef struct {
+    union {
+        xta_xa_resource_t  xa_resource;
+    };
+} xta_mysql_xa_resource_t;
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+
+
+    /**
+     * Create a new object to represent a MySQL XA resource
+     * @param[in] connection to MySQL already opened by the application
+     *            program
+     * @return a new object or NULL in the event of error
+     */
+    xta_mysql_xa_resource_t *xta_mysql_xa_resource_new(MYSQL *connection);
+
+    
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+
+
+/* restore old value of LIXA_TRACE_MODULE */
+#ifdef LIXA_TRACE_MODULE_SAVE
+# undef LIXA_TRACE_MODULE
+# define LIXA_TRACE_MODULE LIXA_TRACE_MODULE_SAVE
+# undef LIXA_TRACE_MODULE_SAVE
+#endif /* LIXA_TRACE_MODULE_SAVE */
+
+
+
+#endif /* MYSQL_XA_RESOURCE_H */
