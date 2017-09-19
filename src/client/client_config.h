0ca7ceec27f9d487781b26c52b2fff3c731920d7
Christian Ferrari <tiian@users.sourceforge.net>
2017-09-19 23:05:51 +0200

    License change from GPL 2 to LGPL 2.1 for all source files related to linkable libraries

--- src/client/client_config.h
+++ src/client/client_config.h
@@ -4,37 +4,17 @@
  *
  * This file is part of LIXA.
  *
- * LIXA is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as published
- * by the Free Software Foundation.
+ * LIXA is free software: you can redistribute this file and/or modify
+ * it under the terms of the GNU Lesser General Public License version 2.1 as
+ * published by the Free Software Foundation.
  *
  * LIXA is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with LIXA.  If not, see <http://www.gnu.org/licenses/>.
- *
- * This file is part of the libraries provided by LIXA.
- * In addition, as a special exception, the copyright holders of LIXA gives
- * Globetom Holdings (Pty) Ltd / 92 Regency Drive / Route 21 Corporate Park /
- * Nellmapius Drive / Centurion / South Africa
- * the permission to redistribute this file and/or modify it under the terms
- * of the GNU Lesser General Public License version 2.1 as published
- * by the Free Software Foundation.
- * The above special grant is perpetual and restricted to
- * Globetom Holdings (Pty) Ltd: IN NO WAY it can be automatically transferred
- * to a different company or to different people. "In no way" contemplates:
- * merge, acquisition and any other possible form of corportate change.
- * IN NO WAY, the above special grant can be assimilated to a patent or
- * any other form of asset.
- *
- * August 1st, 2016: Christian Ferrari thanks Globetom Holdings (Pty) Ltd
- * for its donation to Emergency NGO, an international charity that promotes
- * a culture of peace, solidarity and respect for human rights providing free,
- * high quality medical and surgical treatment to the victims of war, landmines
- * and poverty.
  */
 #ifndef CLIENT_CONFIG_H
 # define CLIENT_CONFIG_H
