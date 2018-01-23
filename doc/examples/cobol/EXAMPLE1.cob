fbfb9059a785285ddacd11ee4cc9dfd2c25f6a34
Christian Ferrari <tiian@users.sourceforge.net>
2018-01-23 22:28:16 +0100

    After copyright year update

--- doc/examples/cobol/EXAMPLE1.cob
+++ doc/examples/cobol/EXAMPLE1.cob
@@ -1,4 +1,4 @@
-      * Copyright (c) 2009-2017, Christian Ferrari 
+      * Copyright (c) 2009-2018, Christian Ferrari 
       * <tiian@users.sourceforge.net>
       * All rights reserved.
       *
--- src/common/lixa_utils.c
+++ src/common/lixa_utils.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2017, Christian Ferrari <tiian@users.sourceforge.net>
+ * Copyright (c) 2009-2018, Christian Ferrari <tiian@users.sourceforge.net>
  * All rights reserved.
  *
  * This file is part of LIXA.
@@ -59,7 +59,7 @@
 void lixa_print_version(FILE *stream)
 {
     fprintf(stream, "LIXA: a LIbre XA implementation\n"
-            "Copyright (c) 2009-2017, Christian Ferrari; "
+            "Copyright (c) 2009-2018, Christian Ferrari; "
             "all rights reserved.\n"
             "License: GPL (GNU Public License) version 2\n"
             "Package name: %s; package version: %s\n"
